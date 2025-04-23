b6c124c5bd4d89c0f0ff08400cb958f5d0896952
Fergo20011 <fergo20011@hotmail.com>
2025-04-23 00:43:47 +0000

    EDA 6

--- src/utils.py
+++ src/utils.py
@@ -83,12 +83,9 @@ def analisis_numerico_numerico(df,y,columnas_excluidas=[],limites=[]):
                 else:
                     sns.histplot(ax=axis[fila,columna],data=df,x=column)
                     sns.boxplot(ax=axis[fila+1,columna],data=df,x=column)
-            else:
-                sns.histplot(ax=axis[fila,columna],data=df,x=column)
-                sns.boxplot(ax=axis[fila+1,columna],data=df,x=column)
-        else:
-            sns.histplot(ax=axis[fila,columna],data=df,x=column)
-            sns.boxplot(ax=axis[fila+1,columna],data=df,x=column)
+
+        
+        
     plt.tight_layout()
     plt.show()
 
